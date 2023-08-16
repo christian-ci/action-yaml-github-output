db9efc58438b970e6a78c14de45a3fe227b2d1c7
christianc <55255417+christian-ci@users.noreply.github.com>
2023-08-16 10:03:24 -0500

    Debugging action dirs

--- action_entrypoint.sh
+++ action_entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -l
 
-python main_action.py \
+echo "Listing /github/workspace/"
+ls -la /github/workspace/
+ls -la /
+ls -la /github/
+
+python /main_action.py \
 --file_path "$FILE_PATH" \
 --format_type "$FORMAT_TYPE" \
 --main_key "$MAIN_KEY" \
