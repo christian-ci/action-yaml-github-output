e792ac01369d3714bce63cd9079793da3d868524
christianc <55255417+christian-ci@users.noreply.github.com>
2023-08-28 15:25:14 -0500

    Reverting and refactoring from scratch

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import yaml
 import os
 import argparse
@@ -9,19 +10,32 @@ def extract_simple_nested(yaml_data, main_key=None, sub_key=None):
         yaml_data = yaml_data.get(sub_key, {})
     return yaml_data
 
-def extract_crud(yaml_data, top_level_keys=None, primary_key=None, primary_value=None):
-    extracted_data = {}
-    if top_level_keys is None:
-        top_level_keys = yaml_data.keys()
-    for key in top_level_keys:
-        section_data = yaml_data.get(key, [])
-        for entry in section_data:
-            if entry.get(primary_key) == primary_value:
-                extracted_data[key] = entry
-    return extracted_data
+def extract_crud(yaml_data, primary_key=None, primary_value=None, top_level_keys=None):
+    output = {}
+    if top_level_keys:
+        top_level_keys = top_level_keys.split(',')
+    else:
+        top_level_keys = [None]
+
+    for top_level_key in top_level_keys:
+        if top_level_key:
+            yaml_data_filtered = yaml_data.get(top_level_key, [])
+        else:
+            yaml_data_filtered = yaml_data
+
+        if not primary_key or not primary_value:
+            output[top_level_key] = yaml_data_filtered
+            continue
+
+        services = yaml_data_filtered.get('services', [])
+        for service in services:
+            if service.get(primary_key) == primary_value:
+                output[top_level_key] = service
+                break
+
+    return output
 
 def main_action():
-    # Set up command-line argument parser
     parser = argparse.ArgumentParser()
     parser.add_argument('--file_path', required=True, help='Path to the yaml file')
     parser.add_argument('--main_key', help='Main key in the YAML to look for nested data', default=None)
@@ -29,32 +43,27 @@ def main_action():
     parser.add_argument('--format_type', help='The format of the YAML. Possible values: simple_nested, crud', default='simple_nested')
     parser.add_argument('--primary_key', help='Primary key in the CRUD format to look for specific settings block', default=None)
     parser.add_argument('--primary_value', help='Value of the primary key in the CRUD format to extract specific settings', default=None)
-    parser.add_argument('--top_level_keys', type=str, help='Top-level keys to search in the YAML (Optional, comma-separated if multiple)')
+    parser.add_argument('--top_level_keys', help='Top-level keys for the CRUD format (Optional, comma-separated)', default=None)
     args = parser.parse_args()
 
-    # Load YAML file
     with open(args.file_path, 'r') as yaml_file:
         yaml_data = yaml.safe_load(yaml_file)
 
-    # Extract desired data
-    if args.format_type == 'crud':
-        top_level_keys = args.top_level_keys.split(",") if args.top_level_keys else None
-        yaml_data = extract_crud(yaml_data, top_level_keys, args.primary_key, args.primary_value)
+    if args.format_type == "crud":
+        yaml_data = extract_crud(yaml_data, args.primary_key, args.primary_value, args.top_level_keys)
     else:
         yaml_data = extract_simple_nested(yaml_data, args.main_key, args.sub_key)
 
-    # Get the GitHub env and output file paths from the environment variables
     github_env_file = os.environ.get('GITHUB_ENV')
     github_output_file = os.environ.get('GITHUB_OUTPUT')
 
-    # Write data to the env and output files
     with open(github_env_file, 'a') as env_file, open(github_output_file, 'a') as output_file:
         for key, value in yaml_data.items():
             env_key = key.upper().replace('-', '_')
             env_file.write(f"{env_key}={value}\n")
             output_file.write(f"{key}={value}\n")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         main_action()
     except Exception as e:
